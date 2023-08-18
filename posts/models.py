d98f385ccb5ea754d600af0a5fbcbfa6a1eb3bcf
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-08-18 18:34:21 +0200

    migrate

--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class post(models.Model):
-    title = models.CharField("Title", max_length=120)
-    content = models.TextField("content",max_length=10000)
+    title = models.CharField(max_length=120)
+    content = models.TextField(max_length=10000)
     publish_date = models.DateTimeField()
 
    
