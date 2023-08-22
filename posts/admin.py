af7591043f8d4bb612204c1327b724bff849a59d
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-08-22 20:42:42 +0200

    add static , tags , comments

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import post
+from .models import post, comment
 
 admin.site.register(post)
+admin.site.register(comment)
+
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.utils import timezone
 
 # Create your models here.
 
@@ -15,8 +16,19 @@ class post(models.Model):
     
     def __str__(self) -> str:
         return self.title
+
+
+    
         
     
     
    
-        
+class comment(models.Model):
+    author  = models.ForeignKey(User,related_name='comment_author',on_delete=models.CASCADE)
+    post = models.ForeignKey(post , related_name='comment_post', on_delete=models.CASCADE)
+    comment = models.TextField(max_length=500)
+    create_date = models.DateTimeField(default= timezone.now)
+
+
+    def __str__(self) -> str:
+        return self.comment
