60034ad723ef83a72fcdea07e070744406b6311a
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-08-22 23:03:17 +0200

    display all posts in html file

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import post
 
 # Create your views here.
+
+def post_list(request):
+    data = post.objects.all()
+    return render(request,'all_posts.html', {'posts' : data})
+
+
+
