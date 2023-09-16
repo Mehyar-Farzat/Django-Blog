68f337aeeb251e4b3271b58ec1b2400e6991a59f
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-16 17:27:45 +0200

    customize admin panel and add editor

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 from .models import post, comment
 
-admin.site.register(post)
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+    list_display = ('title','publish_date', 'author')
+
+admin.site.register(post,PostAdmin)
 admin.site.register(comment)
 
